67090efadbba4f872a21ca1cc36e3173d42bb25e
Timur <mr14mr@yandex.ru>
2021-05-12 21:47:39 +0300

    frontend init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Trading></Trading>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Trading from './components/Trading.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Trading
   }
 }
 </script>
