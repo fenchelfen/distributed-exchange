e6ef8ec01cd7be356783c8a9c811fc5563732b5a
fenchelfen <m.lyamets@innopolis.ru>
2021-05-12 20:29:56 +0500

    Add queue implementation and a somewhat btree

--- eth/contracts/exchange.sol
+++ eth/contracts/exchange.sol
@@ -5,23 +5,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Queue
+contract SwotQueue
 {
-    struct _Queue {
-        uint[] data;
+    struct Queue {
+        bytes32[] data;
         uint front;
         uint back;
+        mapping(bytes32 => Order) hashToOrder;
+    }
+
+    struct Order {
+      address account;
+      uint256 amount;
+    }
+
+    function getOrderId(Order memory order) internal pure returns (bytes32)
+    {
+      return keccak256(abi.encode(order.account, order.amount));
     }
     /// @dev the number of elements stored in the queue.
-    function length(_Queue storage q) view internal returns (uint) {
+    function length(Queue storage q) internal returns (uint) {
         return q.back - q.front;
     }
     /// @dev the number of elements this queue can hold
-    function capacity(_Queue storage q) view internal returns (uint) {
+    function capacity(Queue storage q) internal returns (uint) {
         return q.data.length - 1;
     }
     /// @dev push a new element to the back of the queue
-    function push(_Queue storage q, uint data) internal
+    function push(Queue storage q, bytes32 data) internal
     {
         if ((q.back + 1) % q.data.length == q.front)
             return; // throw;
@@ -29,7 +40,7 @@ contract Queue
         q.back = (q.back + 1) % q.data.length;
     }
     /// @dev remove and return the element at the front of the queue
-    function pop(_Queue storage q) internal returns (uint r)
+    function pop(Queue storage q) internal returns (bytes32 r)
     {
         if (q.back == q.front)
             revert(); // throw;
@@ -37,38 +48,69 @@ contract Queue
         delete q.data[q.front];
         q.front = (q.front + 1) % q.data.length;
     }
+    /// @dev pick a value at the front of the queue 
+    function pick(Queue storage q) internal returns (bytes32 r)
+    {
+        if (q.back == q.front)
+            revert(); // throw;
+        return q.data[q.front];
+    }
+    function pushToQueue(Queue storage q, Order memory order) internal {
+        bytes32 orderId = getOrderId(order);
+        q.hashToOrder[orderId] = order;
+        push(q, orderId);
+    }
+    function pickFromQueue(Queue storage q) internal returns (Order memory) {
+        bytes32 orderId = pick(q);
+        return q.hashToOrder[orderId];
+    }
+    function popFromQueue(Queue storage q) internal returns (Order memory order) {
+        bytes32 orderId = pop(q);
+        order = q.hashToOrder[orderId];
+        q.hashToOrder[orderId] = Order(address(0), 0);
+    }
 }
 
-contract InnoDEX is Ownable, Queue {
+contract SwotOrderBook is SwotQueue 
+{
+  struct OrderBook {
+    OrderBookNode root;
+    OrderBookNode[] nodes;
+  }
+
+  struct OrderBookNode {
+    uint256 left;
+    uint256 right;
+    uint256 parent;
+
+    uint256 maxAmount; 
+    uint256 minAmount;
+    uint256 totalAmount;
+
+    Queue orders; // list of order identifiers (idx)
+    mapping(bytes32 => Order) hashToOrder; 
+  }
+  function getNode(OrderBook storage book, uint256 id) internal returns (OrderBookNode storage) {
+    return book.nodes[id];
+  }
+}
+
+contract InnoDEX is Ownable, SwotQueue, SwotOrderBook {
 
   using SafeMath for uint256;
 
   Token token;
-  OrderBook book;
 
   mapping(address => uint256) public tokenBalances;
 
   constructor(ERC20 tokenContract) {
     token.tokenContract = tokenContract;
-    // token.currentSellPrice = tokenContract.initialPrice;
-  }
 
-  struct Order {
-    address account;
-    uint256 amount;
+    Order memory firstOrder = Order(msg.sender, 1337);
+    pushToQueue(token.bidsOrderBook.root.orders, firstOrder);
   }
 
   enum OrderType { Bid, Ask }
-
-  struct OrderBook {
-    OrderBookNode root;
-  }
-
-  struct OrderBookNode {
-    Queue orders;
-    OrderBookNode[] left;
-    OrderBookNode[] right;
-  }
 
   uint public constant bucketStart = 100;
   uint public constant bucketEnd = 999;
@@ -84,28 +126,20 @@ contract InnoDEX is Ownable, Queue {
   function getTokenContract() public view returns (ERC20) {
     return token.tokenContract;
   }
-
-  function depositEther(uint amount) public payable returns (uint256) {
-    require(token.tokenContract.transferFrom(msg.sender, address(this), amount), 'Failed to transfer tokens to SCA');
-
-    tokenBalances[msg.sender] = tokenBalances[msg.sender].add(amount);
-
-    emit TokenDeposited(msg.sender, block.timestamp, token.tokenContract.symbol(), amount);
-  }
-  
+ 
   /* START OrderBookAPI */
 
   function sayHello() public view returns (string memory) {
     return "Hi There";
   }
-
-  function isSameBucket(uint256 a, uint256 b) public returns (bool, uint256, uint256) {
+  
+  function isSameBucket(uint256 a, uint256 b) public returns (bool) {
     uint256 max;
     uint256 min;
 
-    // if (a == b) {
-    //   return true;
-    // }
+    if (a == b) {
+      return true;
+    }
 
     if (a > b) { max = a; min = b; }
     else { max = b; min = a; }
@@ -114,16 +148,35 @@ contract InnoDEX is Ownable, Queue {
     uint256 t = (max - min) / 10000;
 
     // if integer part is 0, then two amounts are close enough to be in the same bucket
-    return (t == 0, max - min, max - min / 10000);
+    return (t == 0);
   }
 
-  function insertOrder(uint256 amount, OrderType orderType) public view returns (string memory) {
+  function insertOrder(uint256 amount, OrderType orderType) public {
     Order memory order;
     order.account = msg.sender;
     order.amount = amount;
+
+    OrderBookNode storage currentNode = token.bidsOrderBook.root;
     
-    OrderBookNode memory currentNode = book.root;
+    if (orderType == OrderType.Bid) { currentNode = token.bidsOrderBook.root; }
+    else { currentNode = token.asksOrderBook.root; }
+
+
+    if (isSameBucket(order.amount, pickFromQueue(currentNode.orders).amount)) {
+      // todo: push into the root bucket queue
+    }
+
     while (true) {
+      if (isSameBucket(order.amount, pickFromQueue(currentNode.orders).amount)) {
+        // todo: push into this node bucket queue
+        break;
+      }
+
+      if (order.amount > pickFromQueue(currentNode.orders).amount) {
+        // todo: go to the right
+      } else {
+        // todo: go to the left
+      }
     }
   }
   
@@ -146,6 +199,15 @@ contract InnoDEX is Ownable, Queue {
 
   function cancelLimitOrder() public {
   }
+
+  function depositEther(uint amount) public payable {
+    require(token.tokenContract.transferFrom(msg.sender, address(this), amount), 'Failed to transfer tokens to SCA');
+
+    tokenBalances[msg.sender] = tokenBalances[msg.sender].add(amount);
+
+    emit TokenDeposited(msg.sender, block.timestamp, token.tokenContract.symbol(), amount);
+  }
+ 
 
   /* START view functions */
 
--- eth/contracts/token.sol
+++ eth/contracts/token.sol
@@ -21,6 +21,7 @@ contract HELLO {
 }
 
 contract SWOT is ERC20 {
+
   constructor (string memory name, string memory symbol) ERC20(name, symbol) {
     _mint(msg.sender, 9999);
   }
