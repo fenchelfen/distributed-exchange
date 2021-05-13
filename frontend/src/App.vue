910db93166ec807a196c21b3024b23ddd192d18e
Timur <mr14mr@yandex.ru>
2021-05-13 12:27:27 +0300

    working eth buy

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-    <h5>My address: {{eth.userAddress}}</h5>
+    <h5>My address: {{ eth.userAddress }}</h5>
+    <h5>My ETH balance in InnoDEX: {{ eth.innoDexETHBalance }}</h5>
+    <h5>My SWOT balance in InnoDEX: {{ eth.innoDexSWOTBalance }}</h5>
+    <div>
+      <label for="eth">amount of ETH to put</label>
+      <input id="eth" name="eth" v-model="buy_eth.amount">
+      <button v-on:click="putETHOnInnoDex">put ETH on InnoDEX</button>
+    </div>
+    <div>
+      <label for="swot">amount of SWOT to but</label>
+      <input id="swot" name="swot" v-model="buy_swot.amount">
+      <button v-on:click="buySWOTOnInnoDex">get SWOT on InnoDEX</button>
+    </div>
+    <div>
+      <button v-on:click="updateInnoDexBalance">update balances</button>
+
+    </div>
+
     <Chart :points="this.stocks"></Chart>
   </div>
 </template>
@@ -8,6 +25,8 @@
 <script>
 /* eslint-disable */
 import Chart from './components/Chart.vue'
+import {dexAbi} from "./dex-abi";
+import {ethers} from "ethers";
 
 export default {
   name: 'App',
@@ -15,11 +34,6 @@ export default {
     Chart
   },
   mounted() {
-    // The provider
-    const provider = this.$ethereum;
-
-// Connect the provider
-    provider.connect();
 
 // ethereum info
     const {
@@ -34,25 +48,48 @@ export default {
         chainId,
         chainName,
         userAddress,)
-    this.$data.eth = {
-      providerConnected: providerConnected,
-      providerName: providerName,
-      chainId: chainId,
-      chainName: chainName,
-      userAddress: userAddress
-    }
+    this.$data.eth.userAddress = userAddress
 
     this.getOwner();
+
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    console.log(signer);
+
+    // contract connect
+    const address = '0x18b06f4f00197ccc872d909801f28d17493ed889';
+    const dexContract = new ethers.Contract(address, dexAbi, provider);
+    const dexContractWithSigner = dexContract.connect(signer);
+    console.log(dexContractWithSigner);
+    this.eth.dexContractWithSigner = dexContractWithSigner;
+
+    this.updateInnoDexBalance();
+
+
+    // var contract = new Contract(dexAbi, address);
+    // console.log(contract);
+    // let res = contract.methods.depositEther().call();
+    // console.log("res", res);
   },
   data: function () {
 
     return {
+      buy_eth:{
+        amount: 0
+      },
+      buy_swot:{
+        amount: 0
+      },
       eth: {
+        innoDexETHBalance: 0,
+        innoDexSWOTBalance: 0,
         providerConnected: false,
-      providerName: '',
-      chainId: '',
-      chainName: '',
-      userAddress: ''
+        providerName: '',
+        chainId: '',
+        chainName: '',
+        userAddress: '',
+        dexContractWithSigner: null,
       },
       "stocks": [
         {
@@ -79,10 +116,36 @@ export default {
     }
   },
   methods: {
-    getOwner(){
-      ethereum.request({ method: 'eth_accounts' }).then((data) => {
+    getOwner() {
+      ethereum.request({method: 'eth_accounts'}).then((data) => {
         console.log(data);
       }).catch()
+    },
+    updateInnoDexBalance() {
+      console.log("started balance update");
+      this.$data.eth.dexContractWithSigner.getETHBalance().then((balance) => {
+        this.eth.innoDexETHBalance = ethers.utils.formatEther(balance)
+      })
+
+      this.$data.eth.dexContractWithSigner.getSWOTBalance().then((balance) => {
+        this.eth.innoDexSWOTBalance = ethers.utils.formatEther(balance)
+      })
+      console.log("finished balance update");
+
+    },
+    putETHOnInnoDex() {
+      let overrides = {
+        // To convert Ether to Wei:
+        value: ethers.utils.parseEther(this.buy_eth.amount.toString())     // ether in this case MUST be a string
+      };
+      console.log(this.buy_eth.amount)
+
+      console.log(this.$data.eth.dexContractWithSigner.depositEther(overrides).then((el) => {
+        console.log("then", el)
+      }));
+    },
+    buySWOTOnInnoDex(){
+
     }
   }
 
---
+++ frontend/src/dex-abi.js
@@ -0,0 +1,421 @@
+export const dexAbi =[
+    {
+      "inputs": [
+        {
+          "internalType": "contract ERC20",
+          "name": "tokenContract",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "_initiator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "_timestamp",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "_tokenSymbol",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "_amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "EtherDeposited",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "bidder",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "asker",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "OrderExecuted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "_initiator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "_timestamp",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "_tokenSymbol",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "_amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "TokenDeposited",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "etherBalances",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "tokenBalances",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "depositToken",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [],
+      "name": "depositEther",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "tickerBidsExist",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "tickerAsksExist",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "getBestBid",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "getBestAsk",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "placeBidOrder",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "placeAskOrder",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "cancelBidOrder",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "enum InnoDEX.Ticker",
+          "name": "ticker",
+          "type": "uint8"
+        }
+      ],
+      "name": "cancelAskOrder",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "a",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "b",
+          "type": "uint256"
+        }
+      ],
+      "name": "isSameBucket",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "pure",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "getETHBalance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "getSWOTBalance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    }
+  ]
+;
